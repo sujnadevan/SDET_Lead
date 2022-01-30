eac8ebc1a44d32155f7ec5cce93068c1366b3499
sujnadevan <sujnavdevan371@gmail.com>
2022-01-30 22:41:53 +0530

    Second Lead commit

---
+++ src/test/java/DemoTest/CreateDemoTest.java
@@ -0,0 +1,13 @@
+package DemoTest;
+
+import org.testng.annotations.Test;
+
+public class CreateDemoTest {
+	
+	@Test
+	public void demoTest()
+	{
+		System.out.println("Second Commit Lead message");
+	}
+
+}
